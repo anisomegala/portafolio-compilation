c0ef60a873d3e84bf9e9ec415cb4e639d8033cf0
anisomegala <anielsomeillan@icloud.com>
2021-09-29 12:33:33 +0200

    fast updates in bio page

--- src/component/timelineBio/timeline-data.jsx
+++ src/component/timelineBio/timeline-data.jsx
@@ -10,6 +10,16 @@ let timelineData = [
     },
     {
       id: 2,
+      title: "Web Developer",
+      location: "Warsaw, Poland",
+      description:
+        "Freelance web development for Bones Studios Warsaw, Rock Your Cortex, Meskla, Kasia Kaminska Yoga, Up dog Yoga School, etc...",
+      buttonText: "Course Certificate",
+      date: "September 2011",
+      icon: "school",
+    },
+    {
+      id: 3,
       title: "Band leader and Bass player",
       location: "Colombo, Sri lanka",
       description:
@@ -19,53 +29,23 @@ let timelineData = [
       icon: "work",
     },
     {
-      id: 3,
-      title: "Quality Assurance Engineer",
-      location: "South Warren, Geshington",
-      description:
-        "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
-      buttonText: "Company Website",
-      date: "September 2011 - June 2013",
-      icon: "work",
-    },
-    {
       id: 4,
-      title: "Oak Ridge College",
-      location: "South Warren, Geshington",
-      description:
-        "Online Course in Magical Beasts and Wonders of the World - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      buttonText: "Course Certificate",
-      date: "September 2011",
-      icon: "school",
+      title: "Bass Player",
+      location: "Sao Paulo, Brazil",
+      description: 'Band Member of Luedji Luna artist, recording of Um corpo no mundo album, participations in africa and europea tours as well as renomed Montreal Jazz Festival in Canada',        
+      buttonText: "Company Website",
+      date: "April 2016 - August 2019",
+      icon: "work",
     },
     {
       id: 5,
-      title: "Hawking College",
-      location: "Skystead, Craonia",
+      title: "Orquesta Jovem Tom Jobin",
+      location: "Sao Paulo, Brazil",
       description:
-        "College - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+        "Electric and Double bass player at jazz symphonic orquestra Jovem Tom Jobim",
       buttonText: "College Projects",
-      date: "2007 - 2011",
+      date: "September 2015 - March 2016",
       icon: "school",
-    },
-    {
-      id: 6,
-      title: "Marble Hills Grammar School",
-      location: "Dragontail, Ascana",
-      description:
-        "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
-      date: "2003 - 2007",
-      icon: "school",
-    },
-    {
-      id: 7,
-      title: "Conservatory of Music Amadeo Roldan",
-      location: "Havana, Cuba",
-      description:
-        "Bachelor in Arts and Music in the speciality of Classic Guitar and Electric Bass",
-      buttonText: "View Frontend Projects",
-      date: "Setember 2006 - March 2010",
-      icon: "work",
     },
   ];
   
